e9ffffb25f82be1916bf98bee48ad5a46a08ce70
Zicheng Wang <16488024+zikowang@users.noreply.github.com>
2023-12-20 23:41:01 +0100

    feat: add pokemon test

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,18 +1,34 @@
-import { test, expect } from '@playwright/test';
+import { test, expect } from "@playwright/test";
 
-test('has title', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+test("has title", async ({ page }) => {
+  await page.goto("http://localhost:4321/");
 
   // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
+  await expect(page).toHaveTitle(/Astro/);
 });
 
-test('get started link', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+test("Pokemon list link", async ({ page }) => {
+  await page.goto("http://localhost:4321/");
 
-  // Click the get started link.
-  await page.getByRole('link', { name: 'Get started' }).click();
+  // Click the Pokemon link.
+  await page.getByRole("link", { name: "Pokemon" }).click();
 
-  // Expects page to have a heading with the name of Installation.
-  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+  // Expects page to have a heading with the name of List
+  await expect(page.getByRole("heading", { name: "List" })).toBeVisible();
+});
+
+test("Pokemon link", async ({ page }) => {
+  await page.goto("http://localhost:4321/");
+
+  // Click the Pokemon List link.
+  await page.getByRole("link", { name: "Pokemon" }).click();
+
+  // Expects page to have a heading with the name of List
+  await expect(page.getByRole("heading", { name: "List" })).toBeVisible();
+
+  // Click the Bulbasaur link.
+  await page.getByRole("link", { name: "Bulbasaur" }).click();
+
+  // Expects page to have a heading with the name of Bulbasaur
+  await expect(page.getByRole("heading", { name: "Bulbasaur" })).toBeVisible();
 });
