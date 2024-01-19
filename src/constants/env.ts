d01a83908615ab04981f97f13f4ce008378bd967
Zicheng Wang <zicheng.wang@dpa-info.com>
2024-01-19 10:20:14 +0100

    chore: adjust env

--- src/constants/env.ts
+++ src/constants/env.ts
@@ -1,1 +1,1 @@
-export const { PUBLIC_POKEMON_API, PUBLIC_ELECTIONS_LIVE_S3 } = import.meta.env;
+export const { PUBLIC_ELECTIONS_LIVE_S3 } = import.meta.env;
--- src/env.d.ts
+++ src/env.d.ts
@@ -1,7 +1,6 @@
 /// <reference types="astro/client" />
 
 interface ImportMetaEnv {
-    readonly PUBLIC_POKEMON_API: string;
     readonly PUBLIC_ELECTIONS_LIVE_S3: string;
 }
 
